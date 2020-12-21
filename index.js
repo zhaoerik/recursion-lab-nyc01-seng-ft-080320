bc007648a0365afaf1854a9b15506b001beb0b3f
Erik Zhao <erikzhao98@gmail.com>
2020-12-21 22:23:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 // Code your solution here!
 
+function printString(myString) {
+    console.log(myString[0]);
+   
+    if (myString.length > 1) {
+      let mySubString = myString.substring(1, myString.length);
+      printString(mySubString);
+    } else {
+      return true;
+    }
+  }
+
+function reverseString(myString) {
+    console.log(myString[myString.length - 1]);
+    if (myString.length > 0) {
+        let mySubString = myString.substring(0, myString.length - 1)      
+        reverseString(mySubString)
+    } else {
+        return true;
+    }
+}
